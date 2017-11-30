906f1c13006436f931720f6691d6ecf27bacfb75
mayfordm0999 <MayfordM0999@sduhsd.lan>
2017-11-30 11:00:09 -0800

    practiced making variables on my own

--- variables/src/variables/UsingVariables.java
+++ variables/src/variables/UsingVariables.java
@@ -1,5 +1,20 @@
 package variables;
 
-public class UsingVariables {
+public class UsingVariables 
+{
+	public static void main( String[] args)
+	{
+		int variable_one;
+		double variable_two;
+		String variable_three;
+		//define variables
+		variable_one = 113;
+		variable_two = 2.7182;
+		variable_three = "Computer Science";
+		//displaying values
+		System.out.println("This is room # " + variable_one + "");
+		System.out.println("e is close to " + variable_two + "");
+		System.out.println("I am learning a bit about " + variable_three + "");
+	}
 
 }
