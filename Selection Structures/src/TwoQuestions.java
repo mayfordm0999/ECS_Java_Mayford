e9c363341ef4dd8a86bfd4502902bd259b5b85ff
mayfordm0999 <MayfordM0999@sduhsd.lan>
2017-12-06 10:30:23 -0800

    fixed questions

--- Selection Structures/src/TwoQuestions.java
+++ Selection Structures/src/TwoQuestions.java
@@ -10,25 +10,33 @@ public class TwoQuestions
 		System.out.println("TWO QUESTIONS");
 		System.out.println("Think of something, and I'll try and guess what it is");
 		System.out.println("");
-		System.out.println("Question 1) Is it an animal?");
+		System.out.println("Question 1) Is it an animal, vegitable or mineral?");
 		q1 = keyboard.next();
-		System.out.println("Question 2) Is it bigger than a peice of paper?");
+		System.out.println("Question 2) Is it bigger than a breadbox?");
 		q2 = keyboard.next();
-		if( q1.equals ("yes") && q2.equals ("yes") )
+		if( q1.equals ("animal") && q2.equals ("no") )
 		{
-			System.out.println("Is it a dog?");
+			System.out.println("Is it a squirrel?");
 		}
-		if( q1.equals ("no") && q2.equals ("no") )
+		if( q1.equals ("animal") && q2.equals ("yes") )
 		{
-			System.out.println("Is it a pencil?");
+			System.out.println("Is it a moose?");
 		}
-		if( q1.equals ("no") && q2.equals ("yes") )
+		if( q1.equals ("vegetable") && q2.equals ("no") )
 		{
-			System.out.println("Is it a T.V?");
+			System.out.println("Is it a carrot?");
 		}
-		if( q1.equals ("yes") && q2.equals ("no") )
+		if( q1.equals ("vegetable") && q2.equals ("yes") )
 		{
-			System.out.println("Is it a hamster?");
+			System.out.println("Is it a watermelon?");
+		}
+		if( q1.equals ("mineral") && q2.equals ("no") )
+		{
+			System.out.println("Is it a paper clip?");
+		}
+		if( q1.equals ("mineral") && q2.equals ("yes") )
+		{
+			System.out.println("Is it a camaro?");
 		}
 	}
 }
