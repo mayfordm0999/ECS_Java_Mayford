40d9cc6c030d6bcee13b14d970bc1591baac845f
mayfordm0999 <MayfordM0999@sduhsd.lan>
2017-12-11 10:56:29 -0800

    more while loops

--- While Loops/src/NumbGuessCount.java
+++ While Loops/src/NumbGuessCount.java
@@ -1,4 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class NumbGuessCount {
+public class NumbGuessCount
+{
+	public static void main ( String[] args )
+	{
+		Random r = new Random();
+		Scanner k = new Scanner(System.in);
+		
+		int n = 1 + r.nextInt(10), g = 11, c = 1;
+		
+		System.out.println( "I have chosen a number from 1 to 10. Try to guess it." );
+		
+		do
+		{
+			System.out.print( "Your guess: " );
+			g = k.nextInt();
+			
+			if ( g == n )
+			{
+				System.out.println( "That's right! My secret number was " + n + "!" );
+			    System.out.println( "It only took " + c + " tries!");
+			}
+			else
+				System.out.println( "Sorry that is incorrect." );
+			    c++;
+		} while ( g != n );
 
+	}
 }
