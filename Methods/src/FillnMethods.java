bc04f8ac58e38fa43df554e4929e6a355ad24792
mayfordm0999 <MayfordM0999@sduhsd.lan>
2017-12-13 11:32:25 -0800

    changed code so they would be methods

--- Methods/src/FillnMethods.java
+++ Methods/src/FillnMethods.java
@@ -7,106 +7,60 @@ public class FillnMethods
 		// Fill in the method calls where appropriate.
 
 		System.out.println("Watch as we demonstrate methods.");
-
-		System.out.println();
-		System.out.println("I'm going to get a random character from A-Z");
-		char c = '!';
-		// randchar();
-		System.out.println("The character is: " + c );
-
-		System.out.println();
-		System.out.println("Now let's count from -10 to 10");
-		int begin, end;
-		begin = -10;
-		end = 10;
-		// counter();
-		System.out.println("How was that?");
-
-		System.out.println();
-		System.out.println("Now we take the absolute value of a number.");
-		int x, y = 99;
-		x = -10;
-		// abso();
-		System.out.println("|" + x + "| = " + y );
-
-		System.out.println();
-		System.out.println("That's all.  This program has been brought to you by:");
-		// credits();
+		randchar();
+        counter();
+        abso();
+		credits();
 	}
 
-/*
-	public static ???? credits( ???? )
-	// No parameters.
-	{
-		// displays some boilerplate text saying who wrote this program, etc.
-
+	private static void credits() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("That's all.  This program has been brought to you by:");
 		System.out.println();
 		System.out.println("programmed by Sam Cummings");
-		System.out.println("modified by [your name here]");
+		System.out.println("modified by Miranda");
 		System.out.print("This code is distributed under the terms of the standard ");
 		System.out.println("BSD license.  Do with it as you wish.");
 
-		return ??;
 	}
-*/
 
-
-/*
-	public static ???? randchar( ???? )
-	// No parameters.
+	private static void abso() 
 	{
-		// chooses a random character in the range "A" to "Z"
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("Now we take the absolute value of a number.");
+		int x, y = 10;
+		x = -10;
 		
+		System.out.println("|" + x + "| = " + y );
+	}
+
+	private static void counter()
+	{
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("Now let's count from -10 to 10");
+		
+        for( int n = -10; n <= 10; n = n + 1 )
+        {
+        	System.out.print( n + " " );
+        }
+        System.out.println("How was that?");
+	}
+		
+
+	private static void randchar() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("I'm going to get a random character from A-Z");
 		int numval;
 		char charval;
-
-		// pick a random number from 0 to 25
 		numval = (int)(Math.random()*26);
-		// now add that offset to the value of the letter 'A'
 		charval = (char) ('A' + numval);
-
-		return ??;
+		System.out.println("The character is: " + charval );
 	}
-*/
-
-
-/*
-	public static ???? counter( ???? )
-	// Parameters are:
-	//     int start;
-	//     int stop;
-	{
-		// counts from start to stop by ones
-		int ctr;
-
-		ctr = start;
-		while ( ctr <= stop )
-		{
-			System.out.print(ctr + " ");
-			ctr = ctr+1;
-		}
-
-		return ??;
-	}
-*/
-
-
-/*
-	public static ???? abso( ???? )
-	// Parameters are:
-	//     int value;
-	{
-		// finds the absolute value of the parameter
-		int absval;
-
-		if ( value < 0 )
-			absval = -value;
-		else
-			absval = value;
-
-		return ??;
-	}
-*/
 
 }
+
 
